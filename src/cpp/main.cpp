696703d13919c974c68b9df1ead57efa18e58264
Durdana4 <127374217+Durdana4@users.noreply.github.com>
2023-12-29 17:36:49 +0500

    Update main.cpp

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -8,16 +8,9 @@
 
 #include <iostream>
 #include "task_1.cpp"
-#include "task_2.cpp"
-#include "task_3.cpp"
-#include "task_4.cpp"
-#include "task_5.cpp"
-#include "task_6.cpp"
+//#include "task_2.cpp"
+//#include "task_3.cpp"
+//#include "task_4.cpp"
+//#include "task_5.cpp"
+//#include "task_6.cpp"
 
-using namespace std;
-
-int main() {
-    // write your code here
-
-    return 0;
-}
